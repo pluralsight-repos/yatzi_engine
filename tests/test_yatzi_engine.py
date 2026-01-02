958d2ba4b31d1465c4a4cafa1bccc00ff013d560
msg.edson <msg.edson@gmail.com>
2026-01-02 18:37:18 -0300

    test: add tests with mark parametrize

--- tests/test_yatzi_engine.py
+++ tests/test_yatzi_engine.py
@@ -1,8 +1,15 @@
 import pytest
+
 # from yatzi_engine import full_house
 from src.yatzi_engine import full_house
 
 
-def test_full_house():
-    assert full_house([1,1,2,2,2]) == 8
-    # assert full_house([5,5,6,6,6]) == 28
+@pytest.mark.parametrize(
+    "dice, expected_score",
+    [
+        ([1, 1, 2, 2, 2], 8),
+        ([5, 5, 6, 6, 6], 28),
+    ],
+)
+def test_full_house(dice, expected_score):
+    assert full_house(dice) == expected_score
